8b1594da883ba87e7c233e9d19eadee14966998b
Narendra Singh Mahara <narendramahara85131@gmail.com>
2024-03-17 23:38:24 +0545

    practice set 1 solved

---
+++ 03_practiceSet1.js
@@ -0,0 +1,27 @@
+let name = "Narendra";
+let age = 22;
+console.log(name + age);
+console.log(typeof (name + age));
+
+const intro = {
+  name: "Narendra",
+  age: 22,
+  isSingle: true,
+};
+
+// item = "Hello"; // TypeError: Assignment to constant variable.
+intro.class = "Bsc.CSIT"; // can add new properties to the object
+intro.name = "Naren"; // can change the value of the properties
+console.log(intro);
+
+
+// 
+const dict ={
+    eat: "khanu",
+    sleep: "sutnu",
+    go: "janu",
+    come: "aaunu",
+    love: "maya garnu"
+}
+
+console.log(dict);
