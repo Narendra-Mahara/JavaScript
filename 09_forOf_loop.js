df80b1f35edf160d1677a41b345d93bd2a2c1ab5
Narendra Singh Mahara <narendramahara85131@gmail.com>
2024-03-20 23:53:02 +0545

    forOf loop in js learned

---
+++ 09_forOf_loop.js
@@ -0,0 +1,10 @@
+// for of loop is used to iterate through the elements of an array or a string. The syntax of for of loop is:
+// for (variable of iterable)
+// {
+//     // code to be executed
+// }
+
+for(let i of "narendra")
+{
+    console.log(i); // prints each character of the string
+}
