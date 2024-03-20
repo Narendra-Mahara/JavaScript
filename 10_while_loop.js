126cc71861392c5033bc0d6bf2563aa652b88191
Narendra Singh Mahara <narendramahara85131@gmail.com>
2024-03-21 00:15:28 +0545

    while loop in js learned

---
+++ 10_while_loop.js
@@ -0,0 +1,13 @@
+// while loop is used to execute a block of code as long as the condition is true. The syntax of while loop is:
+// while (condition)
+// {
+//     // code to be executed
+// }
+// program to print numbers from 1 to 10 using while loop
+
+let i = 1;
+let num = 10;
+while (i <= num) {
+  console.log(i);
+  i++;
+}
