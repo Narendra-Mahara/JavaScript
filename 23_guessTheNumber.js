937307c27a8bbb2be980f266e910c29a891d1b74
Narendra Singh Mahara <narendramahara85131@gmail.com>
2024-04-03 16:36:01 +0545

    Chances feature added

--- 23_guessTheNumber.js
+++ 23_guessTheNumber.js
@@ -1,4 +1,5 @@
 let score = 0;
+let chances = 0;
 let computer_guess = Math.floor(Math.random() * 100 + 1);
 // console.log(computer_guess);
 const prompt = require("prompt-sync")();
@@ -6,7 +7,8 @@ const prompt = require("prompt-sync")();
 let userInput = prompt("Guess the number");
 userInput = Number.parseInt(userInput);
 console.log(userInput);
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
+  chances = 100 - i;
   score = 100 - i;
   if (userInput === computer_guess) {
     console.log("Hurray! you guessed right!");
@@ -15,10 +17,13 @@ for (let i = 0; i < 100; i++) {
     break;
   } else if (userInput > computer_guess) {
     console.log("Your guess is greater than computer guess");
+    console.log(`Remaining chances are:${chances}`);
+
     userInput = prompt("Another one:");
     userInput = Number.parseInt(userInput);
   } else {
     console.log("Your guess is lesser than computer guess");
+    console.log(`Remaining chances are:${chances}`);
     userInput = prompt("Another one:");
     userInput = Number.parseInt(userInput);
   }
