c8e5c41751dcc95158107b8f7e7be96c7d5e3fea
Narendra Singh Mahara <narendramahara85131@gmail.com>
2024-04-04 07:54:34 +0545

    alert,confirm,prompt learned

--- jsInBrowser/1_alert,confirm,prompt.js
+++ jsInBrowser/1_alert,confirm,prompt.js
@@ -1,4 +1,4 @@
-// Won't be usign in today's forntend sites.
+// Won't be using in today's fornt-end sites.
 alert("You are opening dangereous site"); // donot run in vs code. Only in browser
 confirm("Do you want to go?"); // donot run in vs code. Only in browser
 // const prompt = require("prompt-sync")(); // to run prompt in vs code
