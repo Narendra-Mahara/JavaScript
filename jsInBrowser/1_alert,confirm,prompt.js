3422aa9882a189b6106669edfca32c17af01cd70
Narendra Singh Mahara <narendramahara85131@gmail.com>
2024-04-04 07:52:01 +0545

    Js in browser started

---
+++ jsInBrowser/1_alert,confirm,prompt.js
@@ -0,0 +1,6 @@
+// Won't be usign in today's forntend sites.
+alert("You are opening dangereous site"); // donot run in vs code. Only in browser
+confirm("Do you want to go?"); // donot run in vs code. Only in browser
+// const prompt = require("prompt-sync")(); // to run prompt in vs code
+let a = prompt("ENter your name");
+document.write("Hello ", a);
