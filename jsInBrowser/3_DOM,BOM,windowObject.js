52bab4e614b94058889d4d4840f1892ae17dda65
Narendra Singh Mahara <narendramahara85131@gmail.com>
2024-04-13 17:55:55 +0545

    introduction to DOM,BOM and window object

---
+++ jsInBrowser/3_DOM,BOM,windowObject.js
@@ -0,0 +1,5 @@
+// Window is a global object in client side JavaScript. It represents the browser window or frame. It is the root of the DOM hierarchy and provides the global scope for the document, frames, history, location, navigator, screen, and other objects.
+
+// DOM stands for Document Object Model. It is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects. That way, programming languages can connect to the page.
+
+// BOM stands for Browser Object Model. It provides objects and methods to interact with the browser. The top object is the window object, which represents the browser window. The window object provides access to other objects, like the history object, the screen object, and the location object.
