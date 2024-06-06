d52b5415a8a33bf67752295689914b60187a6bf8
Narendra Mahara <narendramahara85131@gmail.com>
2024-06-06 18:49:51 +0545

    date object learned

---
+++ jsInBrowser/11_Date_object/script.js
@@ -0,0 +1,12 @@
+let d = new Date();
+let hour = d.getHours();
+let minutes = d.getMinutes();
+let seconds = d.getSeconds();
+let da = d.getFullYear();
+
+let adf = d.setFullYear(2004);
+console.log(d);
+console.log(da);
+console.log(adf);
+
+// also used to make clock in browser
