10ab50d98b1fddbad38e7fcb42a725397224e7dc
Narendra Mahara <narendramahara85131@gmail.com>
2024-06-08 14:17:59 +0545

    async and await in js learned

---
+++ jsInBrowser/16_async_await/script.js
@@ -0,0 +1,20 @@
+// >>>>>>>>async is a keyword that is used to define an asynchronous function. It returns a promise that resolves with the value returned by the async function. The await keyword is used to pause the execution of the async function and wait for the promise to be resolved. It can only be used inside an async function.
+// >>>>>>>>await is used to handle the resolved promise. It waits for the promise to be resolved and returns the resolved value.
+
+const hello = async () => {
+  let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Promise resolved");
+    }, 2000);
+  });
+  let q = new Promise((resolve, reject) => {
+    resolve("Promise reselved");
+  });
+  let result = await p;
+  let error = await q;
+  return [result, error];
+};
+
+hello().then((value) => {
+  console.log(value);
+});
