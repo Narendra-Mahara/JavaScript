dced19dd4d8b3f1a2122ad3216d99d14b6091561
Narendra Mahara <narendramahara85131@gmail.com>
2024-06-12 18:12:29 +0545

    additional information added

--- jsInBrowser/23_fetch_API/script.js
+++ jsInBrowser/23_fetch_API/script.js
@@ -8,16 +8,25 @@
 //   console.log(response);
 // });
 
-
 // >>>>>>>>>> code to fetch data after every 2 seconds
-setInterval(() => {
-  fetch("https://goweather.herokuapp.com/weather/ktm")
-    .then((response) => {
-      console.log(response.status);
-      console.log(response.ok);
-      return response.json();
-    })
-    .then((response) => {
-      console.log(response);
-    });
-}, 2000);
+// setInterval(() => {
+//   fetch("https://goweather.herokuapp.com/weather/ktm")
+//     .then((response) => {
+//       console.log(response.status);
+//       console.log(response.ok);
+//       return response.json();
+//     })
+//     .then((response) => {
+//       console.log(response);
+//     });
+// }, 2000);
+
+// Always use async and await to fetch the data
+
+let getFacts = async () => {
+  let p = await fetch("https://cat-fact.herokuapp.com/facts"); // returns promise
+  let response = await p.json(); // .json() make it into readable format
+  console.log(response[0].text); // This only prints first facts
+};
+
+getFacts();
