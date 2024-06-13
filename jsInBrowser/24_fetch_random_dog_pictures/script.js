bfdb6e9c2f0d82ae58ce8ef4ad2f395dca913879
Narendra Mahara <narendramahara85131@gmail.com>
2024-06-13 18:58:02 +0545

    removed unused code

--- jsInBrowser/24_fetch_random_dog_pictures/script.js
+++ jsInBrowser/24_fetch_random_dog_pictures/script.js
@@ -1,13 +1,11 @@
 let button = document.getElementById("btn");
 const container = document.getElementById("images");
-const imageElement = document.createElement("img");
 
 let fetchImage = () => {
   fetch("https://random.dog/woof.json")
     .then((response) => response.json())
     .then((response) => {
       const imageUrl = response.url;
-      imageElement.src = imageUrl;
       container.style.height = "500px";
       container.style.width = "1000px";
       container.style.backgroundImage = `url(${imageUrl})`;
