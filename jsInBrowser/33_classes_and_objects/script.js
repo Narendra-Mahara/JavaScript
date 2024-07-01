f09bf14a12ccc438bc2e1b3e30bff78e4079c12a
Narendra Mahara <narendramahara85131@gmail.com>
2024-07-01 10:28:05 +0545

    constructor in js learned

--- jsInBrowser/33_classes_and_objects/script.js
+++ jsInBrowser/33_classes_and_objects/script.js
@@ -8,12 +8,18 @@
 //    methods
 // }
 
+// constructor is a special method that is called when an object is created. It is used to initialize the object's properties.
+
 class car {
   constructor(name, price) {
     this.name = name;
     this.price = price;
   }
+
+  // method
+  show() {
+    console.log(`Car name is ${this.name} and price is ${this.price}`);
+  }
 }
-let car1 = new car("Audi", "1200000");
-console.log(car1);
-    
+let Narendra = new car("Audi", "1200000");
+Narendra.show();
