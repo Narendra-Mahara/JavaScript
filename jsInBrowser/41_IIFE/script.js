bb661d214abf56d808dd4eae921d1e56ccf7859e
Narendra Mahara <narendramahara85131@gmail.com>
2024-07-10 14:53:23 +0545

    immediately invoked function expressions (IIFE) in js learned

---
+++ jsInBrowser/41_IIFE/script.js
@@ -0,0 +1,12 @@
+// IIFE in javascript is a design pattern which is also known as self-executing anonymous function. It is a function that runs as soon as it is defined. It is a way to execute a function immediately after it is created. It is a common pattern in javascript to create a new scope for variables so that they do not conflict with the global scope. It is also used to create a closure around the entire script so that the variables are not accessible from outside the function.
+//syntax
+// (function(){
+//     // code here
+// })();
+
+
+// example
+(function(){
+    console.log("Hello World");
+
+})();
